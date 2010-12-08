202a9551a89601c63d427e6cfc0f5efba7cd26c7
Simon Twogood <stwogood@siconix.com>
2010-12-08 16:31:08 -0700

    Initial commit of a basic autocomplete textbox control library.

---
+++ WPFAutoCompleteBox/Provider/IAutoCompleteDataProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WPFAutoCompleteBox.Provider
+{
+    public interface IAutoCompleteDataProvider
+    {
+        IEnumerable<string> GetItems(string textPattern);
+    }
+}
